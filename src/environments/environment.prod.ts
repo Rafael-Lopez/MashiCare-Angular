f757a7338bb0e4e5095017094137d2242b96831c
Rafael Lopez <rafael.lopez@bbd.ca>
2021-06-02 23:47:12 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Pending to be updated
-  rooturl : 'http://ec2-3-23-61-217.us-east-2.compute.amazonaws.com:8081'
+  rooturl : 'http://ec2-3-138-191-227.us-east-2.compute.amazonaws.com:8081'
 };
